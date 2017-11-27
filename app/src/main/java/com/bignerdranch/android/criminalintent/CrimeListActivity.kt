7026be79ef5e9c92f97d419037302e1e8cac10cc
Justin Doak <justinhdoak@gmail.com>
2017-11-27 08:00:46 -0500

    Added crime list activity and fragment, changed main activity

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListActivity.kt
@@ -0,0 +1,9 @@
+package com.bignerdranch.android.criminalintent
+
+import android.support.v4.app.Fragment
+
+class CrimeListActivity : SingleFragmentActivity() {
+    override fun createFragment(): Fragment {
+        return CrimeListFragment()
+    }
+}
