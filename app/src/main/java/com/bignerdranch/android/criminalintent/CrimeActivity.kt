4b31c96630c01142ad30f9f7759d46c9e8972419
Justin Doak <justinhdoak@gmail.com>
2017-11-27 07:46:04 -0500

    created abstract class for single fragment activities

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.kt
@@ -1,26 +1,9 @@
 package com.bignerdranch.android.criminalintent
 
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.support.v4.app.FragmentManager
 
-class CrimeActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_crime)
-
-        var fm: FragmentManager = supportFragmentManager
-        var fragment: Fragment? = fm.findFragmentById(R.id.fragment_container)
-
-        if (fragment == null) {
-            fragment = CrimeFragment()
-            fm.beginTransaction()
-                    .add(R.id.fragment_container, fragment)
-                    .commit()
-        }
-
-
+class CrimeActivity : SingleFragmentActivity() {
+    override fun createFragment(): Fragment {
+        return CrimeFragment()
     }
 }
